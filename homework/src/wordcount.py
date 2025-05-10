d7be7482f4cb035ec24c7650e2adbb035be29c6d
arosasg <arosasg@unal.edu.co>
2025-05-10 10:51:35 -0500

    version_final

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,14 +2,12 @@
 # python3 -m homework data/input data/output
 import os
 
-from homework.src._internals import (
-    count_words,
-    parse_args,
-    preprocess_lines,
-    read_all_lines,
-    split_into_words,
-    write_word_counts,
-)
+from homework.src._internals.count_words import count_words
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def main():
